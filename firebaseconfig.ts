6169eff0c54f96ddd332545509f51865ad5c562a
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-04-26 22:18:24 +0000

    feat: firebase working with .env file

--- firebaseconfig.ts
+++ firebaseconfig.ts
@@ -6,14 +6,15 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBFbv8BmvtwAhVpKluFgJ-dJrfeqqrLO7M",
-  authDomain: "black-list-6f99c.firebaseapp.com",
-  projectId: "black-list-6f99c",
-  storageBucket: "black-list-6f99c.appspot.com",
-  messagingSenderId: "584355112579",
-  appId: "1:584355112579:web:6a5f467dcf138ccb1e9db6",
-  measurementId: "G-DXRVN6SJ8H"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
