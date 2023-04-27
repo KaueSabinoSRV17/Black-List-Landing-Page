802941f00fa581d36ef88db5abbfa8cb61bc2288
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-04-27 00:06:09 +0000

    feat: black background

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,11 @@ export default function App() {
   })
   
 
-  return members?.map(member => {
-    return (
-      <ul key={member.email}>
-        <li>{member.name}</li>
-        <li>{member.email}</li>
-        <li>{member.cellphone}</li>
-        <li>{member.instrument}</li>
-        <li>{member.bio}</li>
-      </ul>
-    )
-  })
+  return (
+    <main>
+      <header className='text-white'>
+        <h1>BLACKLIST</h1>
+      </header>
+    </main>
+  )
 }
