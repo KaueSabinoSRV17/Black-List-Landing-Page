a7620ffb964df5829fa77576347f72e57926a83b
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-05-03 00:56:51 +0000

    chore: form function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import { Member, getAllDocsFromCollection } from './backend/database'
+import { Contact } from './backend/types'
+import { sendEmail } from './backend/functions'
 
 
 export default function App() {
   
   const [members, setMembers] = useState<Member[]>()
+  const [contact, setContact] = useState<Contact>({name: '', email: '', phone: '', observation: ''})
 
   async function getMembers() {
     const members = await getAllDocsFromCollection('members')
@@ -15,24 +18,40 @@ export default function App() {
     getMembers()
   })
   
+  function handleInput(event: FormEvent) {
+    const input = event.target as HTMLInputElement
+    const key = input.name
+    const value = input.value
+
+    setContact({...contact, [key]: value})
+  }
+
+  async function handleForm(event: FormEvent) {
+    event.preventDefault()
+    await sendEmail(contact)
+  }
 
   return (
-    <form className='flex flex-col w-fit mx-auto'>
+    <form className='flex flex-col w-fit mx-auto' onSubmit={handleForm}>
       <legend>Tem interesse em nos contratar?</legend>
 
       <label className='flex flex-col w-fit'>
         Seu nome...
-        <input type="text" className='border-black border' />
+        <input type="text" className='border-black border' name='name' onInput={handleInput} />
       </label>
 
       <label className='flex flex-col w-fit'>
         Seu e-mail...
-        <input type="text" className='border-black border'/>
+        <input type="text" className='border-black border' name='email' onInput={handleInput} />
       </label>
 
       <label className='flex flex-col w-fit'>
         Seu telefone...
-        <input type="text" className='border-black border' />
+        <input type="text" className='border-black border' name='phone' onInput={handleInput} />
+      </label>
+
+      <label className="flex flex-col w-fit">
+        <input type="text" className="border-black border" name='observation' onInput={handleInput} />
       </label>
 
       <button className='bg-green-300'>
---
+++ src/backend/functions.ts
@@ -0,0 +1,10 @@
+import { app } from '../../firebaseconfig'
+import { getFunctions, httpsCallable } from 'firebase/functions'
+import { Contact } from './types'
+
+const FUNCTIONS = getFunctions(app)
+
+export async function sendEmail(contact: Contact) {
+  const functionCallable = httpsCallable(FUNCTIONS, 'sendEmail')
+  await functionCallable(contact)
+}
